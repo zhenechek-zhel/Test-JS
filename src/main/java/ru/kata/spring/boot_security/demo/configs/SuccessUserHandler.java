ffbb79704938592d1adfd097c9a5147af6bfe3c2
Evgeniy <zhel186@mail.ru>
2022-02-11 21:48:11 +0300

    second commit

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -16,10 +16,12 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if (roles.contains("ROLE_USER")) {
-            httpServletResponse.sendRedirect("/user");
-        } else if (roles.contains("ROLE_ADMIN")) {
+        if (roles.contains("ADMIN") && roles.contains("USER")) {
             httpServletResponse.sendRedirect("/admin");
+        } else if (roles.contains("ADMIN")) {
+            httpServletResponse.sendRedirect("/admin");
+        } else if (roles.contains("USER")) {
+            httpServletResponse.sendRedirect("/user");
         }
     }
 }
